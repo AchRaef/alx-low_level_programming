58f0a45fbd613824f0e7e3277b3de6952c948301
AchRaf <achrafhani21@gmail.com>
2023-06-20 04:18:00 -0700

    checks for lowercase character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,9 @@
  * _putchar - writes the character c to stdout
  * print_alphabet - prototype to print alphabet lowercase
  * print_alphabet_x10 - prototype to print 10 time alphabet in lowercase
+ * _islower - function that checks for lowercase character
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
