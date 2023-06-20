a6fe6b2b46d438236434e1e95353dd8bcb63a874
AchRaf <achrafhani21@gmail.com>
2023-06-20 04:14:52 -0700

    print 10 time alphabet in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 /**
  * _putchar - writes the character c to stdout
  * print_alphabet - prototype to print alphabet lowercase
+ * print_alphabet_x10 - prototype to print 10 time alphabet in lowercase
  */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
