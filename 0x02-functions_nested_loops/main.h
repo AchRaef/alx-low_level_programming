464f3ec159250d2e8599e01b82e9c30571938cc4
AchRaf <achrafhani21@gmail.com>
2023-06-20 04:24:54 -0700

    checks for alphabetic character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,10 @@
  * print_alphabet - prototype to print alphabet lowercase
  * print_alphabet_x10 - prototype to print 10 time alphabet in lowercase
  * _islower - function that checks for lowercase character
+ * _isalpha - function that checks for alphabetic character
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
