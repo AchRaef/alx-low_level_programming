5fe5909fcf8e1a13ea785436bde443e0cd583f91
AchRaf <achrafhani21@gmail.com>
2023-06-20 05:59:03 -0700

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
  * print_alphabet_x10 - prototype to print 10 time alphabet in lowercase
  * _islower - function that checks for lowercase character
  * _isalpha - function that checks for alphabetic character
+ * _abs - function that computes the absolute value of an integer
  */
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,3 +12,4 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
