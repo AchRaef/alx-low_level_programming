4c5fce7e833d3d44520fcca0097def7ee2b5679d
AchRaf <achrafhani21@gmail.com>
2023-06-22 04:50:01 -0700

    prints the numbers from 0 to 9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,10 @@
  * _isupper - function to  uppercase letters
  * _isdigit - function that checks for a digit
  * mul - function that multiplies two integers
+ * print_numbers - function that prints the numbers from 0 to 9
  *
  */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
