08a401cc293b41983b5cdd1e48aaa8fea2c0913b
AchRaf <achrafhani21@gmail.com>
2023-06-22 05:08:57 -0700

    function that prints 10 times the numbers from 0 to 14

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@
  * print_numbers - function that prints the numbers from 0 to 9
  * _putchar - writes the character c to stdout
  * print_most_numbers - function that prints the numbers, from 0 to 9 without 2 and 4
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
  *
  */
 int _isupper(int c);
@@ -13,3 +14,4 @@ int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
+void more_numbers(void);
