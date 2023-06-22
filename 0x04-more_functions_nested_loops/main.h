3e2a5071fb04591280f605c19d6bd4a95c98b544
AchRaf <achrafhani21@gmail.com>
2023-06-22 04:56:46 -0700

    add putchat to head file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,12 @@
  * _isdigit - function that checks for a digit
  * mul - function that multiplies two integers
  * print_numbers - function that prints the numbers from 0 to 9
+ * _putchar - writes the character c to stdout
+ * print_most_numbers - function that prints the numbers, from 0 to 9 without 2 and 4
  *
  */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
