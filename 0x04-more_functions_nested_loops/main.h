8abe50d287154cff8d707e77cbeee94ef9b05551
AchRaf <achrafhani21@gmail.com>
2023-06-22 04:46:33 -0700

    multiplies two integers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 /**
  * _isupper - function to  uppercase letters
  * _isdigit - function that checks for a digit
+ * mul - function that multiplies two integers
  *
  */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
