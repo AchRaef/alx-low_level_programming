9a90db256eb6aa149f983c0e5ecfd552ff34fa5a
AchRaf <achrafhani21@gmail.com>
2023-06-22 05:13:08 -0700

    draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,5 @@
 /**
- * _isupper - function to  uppercase letters
- * _isdigit - function that checks for a digit
- * mul - function that multiplies two integers
- * print_numbers - function that prints the numbers from 0 to 9
- * _putchar - writes the character c to stdout
- * print_most_numbers - function that prints the numbers, from 0 to 9 without 2 and 4
- * more_numbers - function that prints 10 times the numbers, from 0 to 14
- *
+ * functions to use
  */
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,3 +8,4 @@ void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
