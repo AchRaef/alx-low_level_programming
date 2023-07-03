10d553237c4f9bd41e0134b766eba8437f839bd4
Achraf HANI <achrafhani21@gmail.com>
2023-07-03 04:31:41 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,11 @@
  *Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-{
+}
 	int i = n;
+	int r;
 
-	for (int r = 0; r < i; r++)
+	for (r = 0; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
