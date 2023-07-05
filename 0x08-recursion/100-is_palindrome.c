2d57547705cb34ce14d2198140ec15927f7d08bc
Achraf HANI <achrafhani21@gmail.com>
2023-07-05 05:05:50 -0700

    string is a palindrome or not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 int palind_check(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -12,6 +13,24 @@ int is_palindrome(char *s)
 	if (*s == 0)
 		return (1);
 	return (palind_check(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
 }
 
 /**
