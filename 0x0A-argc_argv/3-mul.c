1ba118b1fbfcbae9cce5dbf348d1e1f16481c8be
Achraf HANI <achrafhani21@gmail.com>
2023-07-07 08:13:29 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,41 +8,45 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	int sign = 1;
+	int sign_c = 0;
+	unsigned int result = 0;
+	int i = 0;
+	int t = 0;
+	int len = 0;
 
 	while (s[len] != '\0')
+	{
 		len++;
-
-	while (i < len && f == 0)
+	}
+	while (i < len)
 	{
 		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
+			sign_c++;
+			i++;
+		}
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			t = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;
+			i++;
+		}
+		if (t == 1)
+		{
+			break;
 		}
 		i++;
 	}
-
-	if (f == 0)
+	if (t == 0)
+	{
 		return (0);
-
-	return (n);
+	}
+	if (sign_c % 2)
+		sign = -1;
+	return (sign * result);
 }
 
 /**
